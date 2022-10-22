8eddf971574a7e1bf067298015e726a0cf6910f5
Saurav Kumar <saurav.kumar@emplay.net>
2022-10-22 06:39:13 +0530

    Termux Text: Disabled websocket and removed from requirements

--- src/nefarious/websocket.py
+++ src/nefarious/websocket.py
@@ -1,7 +1,7 @@
 import json
 import logging
 from django.conf import settings
-from websocket import create_connection
+# from websocket import create_connection
 
 from nefarious.api.serializers import (
     WatchMovieSerializer, WatchTVSeasonSerializer, WatchTVEpisodeSerializer, WatchTVSeasonRequestSerializer, WatchTVShowSerializer)
@@ -12,24 +12,24 @@ ACTION_UPDATED = 'UPDATED'
 ACTION_REMOVED = 'REMOVED'
 
 
-def send_message(action: str, media_type: str, data: dict):
-    logging.info('Sending "{}" websocket message for media type {}'.format(action, media_type))
-    if not settings.DEBUG:
-        try:
-            ws = create_connection(settings.WEBSOCKET_URL, timeout=5)
-            ws.send(json.dumps({
-                'action': action,
-                'type': media_type,
-                'data': data,
-            }))
-        except Exception as e:
-            logging.error('Failed connecting to websocket server: {}'.format(settings.WEBSOCKET_URL))
-            logging.exception(e)
+# def send_message(action: str, media_type: str, data: dict):
+#     logging.info('Sending "{}" websocket message for media type {}'.format(action, media_type))
+#     if not settings.DEBUG:
+#         try:
+#             ws = create_connection(settings.WEBSOCKET_URL, timeout=5)
+#             ws.send(json.dumps({
+#                 'action': action,
+#                 'type': media_type,
+#                 'data': data,
+#             }))
+#         except Exception as e:
+#             logging.error('Failed connecting to websocket server: {}'.format(settings.WEBSOCKET_URL))
+#             logging.exception(e)
 
 
-def send_media_message(action: str, media: WatchMediaBase):
-    media_type, data = get_media_type_and_serialized_watch_media(media)
-    send_message(action, media_type, data)
+# def send_media_message(action: str, media: WatchMediaBase):
+#     media_type, data = get_media_type_and_serialized_watch_media(media)
+#     send_message(action, media_type, data)
 
 
 def get_media_type_and_serialized_watch_media(media) -> tuple:
